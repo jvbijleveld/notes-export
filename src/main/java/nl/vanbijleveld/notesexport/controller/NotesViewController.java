4b28f61b5836a773188fa13be24c2679d5c2e2e5
Jeffrey <jeffrey@vanbijleveld.nl>
2018-06-01 14:35:46 +0200

    first code for NotesViews

--- src/main/java/nl/vanbijleveld/notesexport/controller/NotesViewController.java
+++ src/main/java/nl/vanbijleveld/notesexport/controller/NotesViewController.java
@@ -1,9 +1,13 @@
 package nl.vanbijleveld.notesexport.controller;
 
+import java.util.List;
+
+import nl.vanbijleveld.notesexport.entities.NotesView;
 import nl.vanbijleveld.notesexport.service.NotesViewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +16,11 @@ public class NotesViewController {
     @Autowired
     NotesViewService viewService;
 
+    @ResponseBody
     @RequestMapping("/views")
-    public String getViewList() {
-        // ToDo: fetch views
-        return null;
+    public List<NotesView> getViewList() {
+        return viewService.getAllViews();
+
     }
 
 }
