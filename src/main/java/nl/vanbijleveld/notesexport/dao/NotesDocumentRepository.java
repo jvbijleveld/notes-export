b731f99953459148bc61291f171eba028da011ad
Jeffrey <jeffrey@vanbijleveld.nl>
2018-06-04 10:25:11 +0200

    debugged view query

--- src/main/java/nl/vanbijleveld/notesexport/dao/NotesDocumentRepository.java
+++ src/main/java/nl/vanbijleveld/notesexport/dao/NotesDocumentRepository.java
@@ -4,29 +4,23 @@ import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class NotesDocumentRepository {
     private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
-    
+
     @Autowired
     JdbcTemplate jdbcTemplate;
-    
-    public void executeViewQuery(String query){
+
+    public void executeViewQuery(String query) {
         LOGGER.info("Execute query {}", query);
-        List<Map> rows = jdbcTemplate.queryForList(query, new SluttyRowMapper());
-       
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(query, new SluttyRowMapper());
+
     }
-    
+
 }
--- src/main/java/nl/vanbijleveld/notesexport/dao/SluttyRowMapper.java
+++ src/main/java/nl/vanbijleveld/notesexport/dao/SluttyRowMapper.java
@@ -1,22 +1,24 @@
 package nl.vanbijleveld.notesexport.dao;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-public class SluttyRowMapper implements RowMapper{
-    
-    public Object mapRow(ResultSet rs) throws SQLException {
+public class SluttyRowMapper implements RowMapper<Object> {
+
+    @Override
+    public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
         ResultSetMetaData rsmd = rs.getMetaData();
-        
-        for (int i = 1; i <= rsmd.getColumnCount(); i++){
+
+        for (int i = 1; i <= rsmd.getColumnCount(); i++) {
             String name = rsmd.getColumnName(i);
             System.out.println("Found column " + name);
         }
-        
+
         return new Object();
-        
+
     }
+
 }
